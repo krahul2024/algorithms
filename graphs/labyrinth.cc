aaf0f78c7b18d2a2b5c14a2668f08fe0a176d09e
krahul2024 <culerrahul2024@gmail.com>
2026-05-08 12:01:24 +0530

    labyrinth and gitignore

--- graphs/labyrinth.cc
+++ graphs/labyrinth.cc
@@ -1,5 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
+
+bool is_same_pair (const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+    return p1.first == p2.first && p1.second == p2.second;
+}
+
+bool is_valid (const int x, const int y, const int n, const int m) {
+    return (x >= 0 && y >= 0 && x < n && y < m);
+}
+
+char get_dir_char (const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+    char dir = '.';
+    if (p2.first < p1.first) {
+        dir = 'D';
+    } else if (p2.first > p1.first) {
+        dir = 'U';
+    } else if (p2.second < p1.second) {
+        dir = 'R';
+    } else if (p2.second > p1.second) {
+        dir = 'L';
+    }
+
+    return dir;
+}
 
 int main() {
     int n, m;
@@ -8,13 +32,12 @@ int main() {
     std::pair<int, int> start, end;
     char matrix [n][m];
     bool vis[n][m];
-    int dist[n][m];
+    std::pair<int, int> parent[n][m];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             std::cin >> matrix[i][j];
             vis[i][j] = false;
-            dist[i][j] = 0;
 
             if (matrix[i][j] == 'A')
                 start = {i, j};
@@ -23,30 +46,51 @@ int main() {
         }
     }
 
-    std::queue<std::pair<std::pair<int, int>, std::pair<int, int>>> q;
-    q.push ({ start, { -1, -1 }});
+    std::queue<std::pair<int, int>> q;
+    q.push (start);
+    parent[start.first][start.second] = start;
+    vis[start.first][start.second] = true;
+
+    bool found = false;
 
     while (!q.empty()) {
-        auto [idx, dist_par] = q.front();
+        auto [x, y] = q.front();
         q.pop();
 
-        auto [x, y] = idx;
-        if (x >= 0 && y >= 0 && x < n && y < m && matrix[x][y] != '#' && !vis[x][y]) {
-            vis[x][y] = true;
-            path.push_back(chr);
-            printf ("[%d][%d]=%c: %c --> ", x, y, chr, matrix[x][y]);
+        if (matrix[x][y] == 'B') {
+            found = true;
+            break;
+        } else {
+            for (int k = -1; k <= 1; k += 2) {
+                int xk = x, yk = y+k;
+                if (is_valid (xk, yk, n, m) && !vis[xk][yk] && matrix[xk][yk] != '#') {
+                    vis[xk][yk] = true;
+                    parent[xk][yk] = { x, y };
+                    q.push ({ xk, yk });
+                }
 
-            if (matrix[x][y] == 'B') {
-                found = true;
-                break;
-            } else {
-                q.push ({{ x, y+1 }, 'R'});
-                q.push ({{ x, y-1 }, 'L'});
-                q.push ({{ x+1, y }, 'D'});
-                q.push ({{ x-1, y }, 'U'});
+                xk = x+k, yk = y;
+                if (is_valid (xk, yk, n, m) && !vis[xk][yk] && matrix[xk][yk] != '#') {
+                    vis[xk][yk] = true;
+                    parent[xk][yk] = { x, y };
+                    q.push ({ xk, yk });
+                }
             }
         }
     }
 
-    printf ("%s\n%zu\n%s\n", (found? "YES": "NO"), path.size(), path.data());
+    if (found) {
+        std::pair<int, int> vert = end;
+        std::string path;
+        while (!is_same_pair (parent[vert.first][vert.second], vert)) {
+            const char dir_char = get_dir_char (vert, parent[vert.first][vert.second]);
+            path.push_back (dir_char);
+            vert = parent[vert.first][vert.second];
+        }
+
+        std::reverse(path.begin(), path.end());
+        std::cout << "YES\n" << path.size() << '\n' << path << '\n';
+    } else {
+        std::cout << "NO\n";
+    }
 }
