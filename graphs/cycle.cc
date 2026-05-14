95668ba07498e1b110d4d5dd5c424bbfb1ed8a9f
krahul2024 <culerrahul2024@gmail.com>
2026-05-14 11:55:42 +0530

    cycle detection simplest method added

--- graphs/cycle.cc
+++ graphs/cycle.cc
@@ -1,6 +1,7 @@
 #include<cstdint>
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using ull = uint64_t;
 using ll = int64_t;
@@ -24,4 +25,34 @@ int main() {
         G[y].push_back (x);
     }
 
+    Vec<bool> vis (n+1, false);
+    bool cycle_found = false;
+
+    std::function<void(int, int)> has_cycle = [&](int u, int p) -> void {
+        vis[u] = true;
+
+        for (const auto& v: G[u]) {
+            if (vis[v] && v != p) {
+                cycle_found = true;
+                break;
+            } else if (!vis[v]){
+                has_cycle (v, u);
+            }
+        }
+    };
+
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i])
+            has_cycle (i, -1);
+
+        if (cycle_found) {
+            std::cout << "Cycle Found\n";
+            break;
+        }
+    }
+
+    if (!cycle_found)
+        std::cout << "No Cycle found\n";
 }
+
+// NOTE: If an undirected graph is not tree(#vertices = n, #edges(m) = n-1), then it contains at least one cycle.
